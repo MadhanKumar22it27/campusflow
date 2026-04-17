af5e2be48296f1c01dd5012859f26e2df1b4964f
MadhanKumar22it27 <mathanbhuvana24@gmail.com>
2026-04-17 17:06:05 +0530

    fix: validate the default checkbox

--- campusflow/fees_&_finance/doctype/fee_structure/fee_structure.js
+++ campusflow/fees_&_finance/doctype/fee_structure/fee_structure.js
@@ -1,8 +1,34 @@
 // Copyright (c) 2026, Madhan and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Fee Structure", {
-// 	refresh(frm) {
+frappe.ui.form.on("Fee Structure", {
+	default(frm) {
+		if (!frm.doc.default) return;
 
-// 	},
-// });
+		frappe.db
+			.get_value(
+				"Fee Structure",
+				{
+					program: frm.doc.program,
+					default: 1,
+					name: ["!=", frm.doc.name],
+				},
+				"name"
+			)
+			.then((r) => {
+				if (!r.message) return;
+
+				frappe.confirm(
+					__("Another default exists. Replace it?"),
+					() => {
+						frappe.db.set_value("Fee Structure", r.message.name, "default", 0);
+						frm.save();
+					},
+					() => {
+						frm.set_value("default", 0);
+						frm.save();
+					}
+				);
+			});
+	},
+});
